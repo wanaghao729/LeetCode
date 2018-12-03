33dc006c9200186ce0ed7987da4ac5e34703518b
wanaghao729 <1090305486@qq.com>
2018-12-03 21:19:09 +0800

    914

--- src/Array/Topic914.java
+++ src/Array/Topic914.java
@@ -1,4 +1,37 @@
 package Array;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Topic914 {
+
+    public boolean hasGroupsSizeX(int[] deck) {
+        int N = deck.length;
+        int[] count = new int[10000];
+        for (int c: deck)
+            count[c]++;
+
+        List<Integer> values = new ArrayList();
+        for (int i = 0; i < 10000; ++i)
+            if (count[i] > 0)
+                values.add(count[i]);
+
+        search: for (int X = 2; X <= N; ++X)
+            if (N % X == 0) {
+                for (int v: values)
+                    if (v % X != 0)
+                        continue search;
+                return true;
+            }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,4,3,2,1};
+        System.out.println(new Topic914().hasGroupsSizeX(array));
+    }
+
 }
