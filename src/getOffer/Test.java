ebbc96b8af93255a9a9d104eb1a4eaf2f2a8ec4e
wanaghao729 <1090305486@qq.com>
2019-07-23 20:53:23 +0800

    链表

---
+++ src/getOffer/Test.java
@@ -0,0 +1,46 @@
+package src.getOffer;
+
+public class Test {
+    private int value = 1;
+    private synchronized void printOdd() {
+        while (value <= 100) {
+            if ((value&0x01) == 1) {
+                System.out.println(Thread.currentThread() + ": -" + value++);
+                this.notify();
+            }else {
+                try {
+                    this.wait();
+                }catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private synchronized void printEven() {
+        while (value <= 100) {
+            if ((value&0x01) == 0) {
+                System.out.println(Thread.currentThread() + ": -" + value++);
+                this.notify();
+            }else {
+                try {
+                    this.wait();
+                }catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException{
+        System.out.println(2&0x1);
+        System.out.println(3&0x1);
+        Test t = new Test();
+        Thread t1 = new Thread(t::printOdd);
+        Thread t2 = new Thread(t::printEven);
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+    }
+}
