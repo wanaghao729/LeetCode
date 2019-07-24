8923617a48e9cc273083622a92b8665d65f3e085
wanaghao729 <1090305486@qq.com>
2019-07-24 21:46:27 +0800

    树

--- src/getOffer/Topic6_62.java
+++ src/getOffer/Topic6_62.java
@@ -19,5 +19,24 @@ public class Topic6_62<E> {
         }
     }
 
+    private TreeNode<E> reConstructBinaryTree(E[] preOrder, E[] inOrder, int length) {
+        if (preOrder == null || inOrder == null || length <= 0)
+            return null;
+        return reConstructBinaryTree(preOrder, 0, length-1, inOrder, 0, length-1);
+    }
 
+    private TreeNode<E> reConstructBinaryTree(
+            E[] preOrder, int startPre, int endPre, E[] inOrder, int startIn, int endIn
+    ) {
+        E value = preOrder[0];
+        this.root = new TreeNode<>(value, null, null);
+        this.root.leftChild = null;
+        this.root.rightChild = null;
+        if (startPre == endPre) {
+            if (startIn == endIn && startPre == startIn)
+                return root;
+            else
+                return null;
+        }
+    }
 }
