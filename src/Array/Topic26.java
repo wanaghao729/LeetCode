50e7c449d941e995b115d8e1bb618782ffa39cbd
wanaghao729 <1090305486@qq.com>
2019-11-22 23:36:02 +0800

    老胡沙雕

--- src/Array/Topic26.java
+++ src/Array/Topic26.java
@@ -1,23 +1,49 @@
 package src.Array;
 
 /**
- * 26、从有序序列移除重复值，返回最终的数组长度，不允许申请多于的内存空间
- *n = [1,1,2] ==> [1,2] 长度为2
- *n = [0,0,1,1,1,2,2,3,3,4] ==> [0,1,2,3,4] 长度为5
+ * 26. Remove Duplicates from Sorted Array
+ * Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
+ *
+ * Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+ *
+ * Example 1:
+ *
+ * Given nums = [1,1,2],
+ *
+ * Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+ *
+ * It doesn't matter what you leave beyond the returned length.
+ * Example 2:
+ *
+ * Given nums = [0,0,1,1,1,2,2,3,3,4],
+ *
+ * Your function should return length = 5, with the first five elements of nums being modified to 0, 1, 2, 3, and 4 respectively.
+ *
+ * It doesn't matter what values are set beyond the returned length.
+ * Clarification:
+ *
+ * Confused why the returned value is an integer but your answer is an array?
+ *
+ * Note that the input array is passed in by reference, which means modification to the input array will be known to the caller as well.
+ *
+ * Internally you can think of this:
+ *
+ * // nums is passed in by reference. (i.e., without making a copy)
+ * int len = removeDuplicates(nums);
+ *
+ * // any modification to nums in your function would be known by the caller.
+ * // using the length returned by your function, it prints the first len elements.
+ * for (int i = 0; i < len; i++) {
+ *     print(nums[i]);
+ * }
  */
 public class Topic26 {
     public int removeDuplicates(int[] nums) {
-        if (nums==null || nums.length==0) return 0;
-        if (nums.length < 2) return 1;
-        int count = 0;
-        int number = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == nums[i-1])
-                continue;
-            else
-                count++;
-        }
-        return count;
+        int i = nums.length > 0 ? 1 : 0;
+        for (int n : nums)
+            if (n > nums[i-1])
+                nums[i++] = n;
+        return i;
     }
 
 
