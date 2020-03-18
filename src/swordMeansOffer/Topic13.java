be52f3f12c7c4a9097832620390f5816c3181a6d
wanaghao729 <1090305486@qq.com>
2020-03-18 20:26:45 +0800

    123

--- src/swordMeansOffer/Topic13.java
+++ src/swordMeansOffer/Topic13.java
@@ -12,9 +12,17 @@ public class Topic13 {
         return dfs(0, 0, 0, 0);
     }
 
+    /**
+     *
+     * @param i 行
+     * @param j 列
+     * @param si    数位和
+     * @param sj
+     * @return
+     */
     private int dfs(int i, int j, int si, int sj) {
         if (i < 0 || i >= m || j < 0 || j >= n || k < si + sj || visited[i][j]) return 0;
         visited[i][j] = true;
-        return 1 + dfs()
+        return 1 + dfs(i + 1, j, (i+1) % 10 != 0 ? si + 1 : si - 8, sj) + dfs(i, j + 1, si, (j + 1) % 10 != 0 ? sj + 1 : sj - 8);
     }
 }
